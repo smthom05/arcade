22db1c8d9dd09a1e8c38acdb1ba3be41ca048856
Scott Thomas <smthom05@gmail.com>
2019-04-26 16:07:22 -0600

    both update and delete routes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var Game = require('../../../models').Game;
 
-
+// index
 router.get("/", function(req, res, next) {
   Game.findAll()
     .then(games => {
@@ -14,6 +14,8 @@ router.get("/", function(req, res, next) {
       res.status(500).send({error})
     });
 });
+
+// show
 
 router.get('/:id', function(req, res, next) {
   Game.findOne({
@@ -31,7 +33,7 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-
+// create
 router.post("/", function(req, res, next) {
   Game.create({
           title: req.body.title,
@@ -47,6 +49,48 @@ router.post("/", function(req, res, next) {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({ error });
     });
+});
+
+// update
+router.put('/:id', function(req, res, next) {
+  Game.update({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  },
+  {
+    returning: true,
+    where: {
+      id: parseInt(req.params.id)
+      }
+    }
+  )
+  .then(([rowsUpdate, [updatedGame]])=> {
+    res.setHeader("Content-Type", "application/json");
+    res.status(202).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+});
+
+// delete
+router.delete('/:id', function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(204).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
 });
 
 module.exports = router;
